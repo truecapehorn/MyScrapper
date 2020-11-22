30e9be7aa219edca22868e3475f296f0dfb73103
truecapehorn <truecapehorn@gmail.com>
2020-11-22 16:22:10 +0100

    uruchamienie wyslania przez emial

--- main.py
+++ main.py
@@ -1,39 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import yagmail
+import os
 
+email = os.environ.get('EMAIL_USER2')
+password = os.environ.get("EMAIL_PASSWORD2")
 
 def send_email(body):
-    import yagmail
-
-    receiver = "tito02@o2.pl"
+    receiver = 'jablonski.norbert@gmail.com'
     try:
-        yag = yagmail.SMTP("jablonski.norbert@gmail.com")
-        yag.send(to=receiver, subject="Cena TV Boxa", contents=body, )
+
+        yag = yagmail.SMTP(user=email,password=password)
+        yag.send(to=receiver, subject="Alert cenowy", contents=body, )
         print("Mail wysłany")
     except Exception as e:
         print("Problem z wyslaniem meila: {}".format(e))
 
 
-def check_price(price_float):
-    if price_float < 350:
-        email = """
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Title</title>
-    </head>
-    <body>
-        <h2>Paraser wykryl ze cena tv boxa jest optymalna:</h2>
-        <h3>Jego cena to <b>{} zl</b></h3>.
-    </body>
-    </html>""".format(price_float)
-        print(email)
-        send_email(email)
+
+def check_price(prices):
+    body=""
+    for k,v in prices.items():
+        b=f"url= {v['url']} , cena = {v['price']} zł.\n"
+        body=body+b
+    print(body)
+
+    # send_email(body)
 
 
-# check_price(price_float)
+
 
 def price_preparation(price):
     price_number = re.sub("[\n\t\s]*", "", price)  # usuniecie bialych znakow
@@ -50,7 +46,7 @@ def parase(url,container,selectors,selec_name):
         soup = BeautifulSoup(page.content, 'html.parser')
         price = soup.find(container, {selectors: selec_name})
         price_number = price_preparation(price.text)
-        return print(f"Cena w {url}: - {price_number} zł")
+        return price_number
 
     except Exception as e:
         print(url, e)
@@ -59,13 +55,22 @@ def parase(url,container,selectors,selec_name):
 url1 = 'https://cumulus.equipment/pl_pl/spiwory/quilty-komfortery.html'
 url2 = 'https://kross.eu/pl/rowery/szosowe/gravel/esker-4-0-zielony-czarny-polysk'
 url3 = 'https://www.merida-bikes.com/pl-pl/bike/667/silex-400'
+url4 = 'https://rower.com.pl/merida-silex-400--2079478'
+url5 = 'http://www.megastart.pl/rowery/turystyczne/silex/107-silex-400'
 
 
 
-parase(url1,"span","id","product-price-78")
-parase(url2,"div","class","a-price")
-parase(url3,"span","class","price")
+cena1=parase(url1,"span","id","product-price-78")
+cena2=parase(url2,"div","class","a-price")
+cena3=parase(url3,"span","class","price")
+cena4=parase(url4,"span","class","base-price")
+cena5=parase(url5,"span","class","price")
 
-
-test=price_preparation("1234,85")
-print(test)
+dane={
+    "cena1":{"price":cena1,"url":url1},
+    "cena2":{"price":cena2,"url":url2},
+    "cena3":{"price":cena3,"url":url3},
+    "cena4":{"price":cena4,"url":url4},
+    "cena5":{"price":cena5,"url":url5},
+      }
+check_price(dane)
