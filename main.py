1cb1daf469b066d181049bfae234a858c71017c1
truecapehorn <truecapehorn@gmail.com>
2020-12-11 18:17:38 +0100

    dodanie czasu uruchomienia

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import datetime
 from pandas_operations import piv, df_produkty
 
 now = datetime.date.today()  # data do pliku
+dtime = datetime.datetime.now()
 
 email = os.environ.get('EMAIL_USER2')
 password = os.environ.get("EMAIL_PASSWORD2")
 
 if email == None or password == None:
-    print(f"email:{email}; password:{password}")
+    print(f"email:{email}; password:{password}; time: {dtime}")
     exit(0)
 
 
@@ -21,15 +22,15 @@ def send_email(body):
         yag = yagmail.SMTP(user=email, password=password)
         yag.send(to=receiver, subject="Alert cenowy", contents='mail.html',
                  attachments=['Fig/graph.png',"Dane/all_data.csv"])
-        print("Mail wysłany")
+        print(f"Mail wysłany - {dtime}")
     except Exception as e:
-        print("Problem z wyslaniem meila: {}".format(e))
+        print("{} Problem z wyslaniem meila: {}".format(dtime,e))
 
 
 def write_to_file(content):
     with open('mail.html', 'w') as f:
         print(content, file=f)
-    print("Mail zapisany")
+    print(f"Mail zapisany - {dtime}")
 
 
 def prepare_mail(content):
